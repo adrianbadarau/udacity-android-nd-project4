a2786dd6af83edf810899e93361355b77dbc6812
abadarau <abadarau@lenovo.com>
2018-03-27 08:09:35 +0200

    added back button to the music list

--- app/src/main/java/com/abadarau/mymusicplayer/MusicListActivity.java
+++ app/src/main/java/com/abadarau/mymusicplayer/MusicListActivity.java
@@ -1,8 +1,11 @@
 package com.abadarau.mymusicplayer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import com.abadarau.mymusicplayer.adapters.SongAdapter;
@@ -20,5 +23,14 @@ public class MusicListActivity extends AppCompatActivity {
         SongAdapter songList = new SongAdapter(this,R.layout.music_list_item, songs);
         ListView songListView = findViewById(R.id.lv_song_list);
         songListView.setAdapter(songList);
+
+        Button backBtn = findViewById(R.id.go_to_main_btn);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
